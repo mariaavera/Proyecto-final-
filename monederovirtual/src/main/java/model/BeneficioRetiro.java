ca9b64730ec0a604e42c68121e7b110adf3173ba
isabellavalenciag <isabella.valenciag@uqvirtual.edu.co>
2026-02-14 21:53:21 -0500

    ejemplo pull
    This reverts commit c31a065b

--- monederovirtual/src/main/java/model/BeneficioRetiro.java
+++ monederovirtual/src/main/java/model/BeneficioRetiro.java
@@ -7,6 +7,10 @@ public class BeneficioRetiro extends Beneficio implements Puntos{
         super("1 mes sin cargos por retiros", 500);
     }
 
+    /**Sobre escribo el metodo aplicar beneficio
+     *para ejecutarlo despues
+     * @param cliente
+     */
     @Override
     public void aplicarBeneficio(Cliente cliente) {
         cliente.setRetirosGratisHasta(LocalDate.now().plusMonths(1));
